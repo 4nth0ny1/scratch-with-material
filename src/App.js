0d67da001405f12ea11148c8eb13f30dc9115dde
4nth0ny1 <acatullo4@gmail.com>
2023-08-20 23:11:23 -0500

    added roboto and tourcard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TourCard from "./components/TourCard";
+
 export default function App() {
   return (
     <div>
       <h1>Scratch with Material UI</h1>
+      <TourCard />
+      <Button variant="contained" color="secondary">
+        Hello world
+      </Button>
     </div>
   );
 }
---
+++ src/components/TourCard.js
@@ -0,0 +1,5 @@
+import Paper from "@mui/material/Paper";
+
+export default function TourCard() {
+  return <Paper elevation={3}>Tour Card</Paper>;
+}
