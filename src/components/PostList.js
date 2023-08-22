34855cd98d02cf9536d96057d1c57f38c80b4f03
4nth0ny1 <acatullo4@gmail.com>
2023-08-21 20:35:52 -0500

    added working repsonsive grid to postlist component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -10,15 +10,25 @@ export default function PostList({ posts }) {
       {posts.map((post) => {
         return (
           <>
-            <Grid item xs={3}>
-              <Paper elevation={10} key={post.id}>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <Paper
+                elevation={10}
+                key={post.id}
+                sx={{ minHeight: "325px" }}
+                rounded
+              >
                 <img
                   className="img"
                   src="https://images.unsplash.com/photo-1533094602577-198d3beab8ea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
                   alt="niagara.jpg"
                 />
                 <Box paddingX={1}>
-                  <Typography>{post.title}</Typography>
+                  <Typography component="h2" variant="h6">
+                    {post.title}
+                  </Typography>
+                  <Typography component="p" variant="body2">
+                    {post.body}
+                  </Typography>
                 </Box>
               </Paper>
             </Grid>
