10f65d213cbe63310bf5c1ca8acbb415fe0ee760
4nth0ny1 <acatullo4@gmail.com>
2023-08-27 19:06:55 -0500

    added responsive box to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,20 @@
+import { Box } from "@mui/material/";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <Box
+      sx={{
+        width: "500px",
+        height: "500px",
+        background: {
+          xs: "pink",
+          sm: "lightgreen",
+          md: "lightblue",
+          lg: "blue",
+        },
+      }}
+    >
+      Home Page
+    </Box>
+  );
 }
